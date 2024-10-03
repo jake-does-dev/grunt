759d0c449ca9e1e33ee67615fb3bc9c1c7665017
Jake Allan <jjemail1995@gmail.com>
2024-10-03 19:40:30 +0100

    feat(setup) improve setup steps

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gleam deps update
+cd build/packages/gpb
+make
+cd ../../..
+./build/packages/gpb/bin/protoc-erl -I. mumble.proto
+erlc -I build/packages/gpb/include mumble.erl
+mv mumble.erl src/
+mv mumble.hrl src/
+cp build/packages/gpb/include/gpb.hrl src/ # required, or else we get a lot of `There was a problem running the shell command 'escript'`... unsure why
